c8a0427e3ac132665b92b5f396ab27c7f82af7ce
reza drakhshan <github2reza@gmail.com>
2024-10-10 12:46:37 +0330

    feat
    Swith on Postgre database

--- config/db/database.py
+++ config/db/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
  
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db.sqlite"
+SQLALCHEMY_DATABASE_URL = "postgresql://root:pwC1uoiCMRHqj3rOfvtoPhWJ@tarkhineh-db:5432/postgres"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
  
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
  
